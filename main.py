e999c58058a2988d3e9c82e4561a580e1f555524
lickelon <jindoman0@naver.com>
2024-11-30 03:22:15 +0900

    feat: crop all input file

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 from modules.kice_cropper import KiceCropper
+from pathlib import Path
 if __name__ == '__main__':
     kc = KiceCropper()
-    kc.extract_problems_to_original('input/2025학년도 대수능.pdf', 10)
+    folder_path = Path('input')
+    pdf_files = folder_path.glob('*.pdf')
+    for pdf_file in pdf_files:
+        pdf_src_path = str(pdf_file)
+        item_code = pdf_file.stem
+        print(item_code)
+        kc.extract_problems_to_original(pdf_name=pdf_src_path, accuracy=10)

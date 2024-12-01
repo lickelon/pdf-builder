22c10c0a0ad2f6ad31279d49d75bfe67e3949fbc
unknown <jindoman0@naver.com>
2024-12-02 01:21:04 +0900

    fix: kice cropper logic modify

--- modules/kice_cropper.py
+++ modules/kice_cropper.py
@@ -11,6 +11,7 @@ from utils.component_info import ComponentInfo
 class KiceCropper:
     def __init__(self, pdf_name: str) -> None:
         self.pdf_name = pdf_name
+        self.base_name = os.path.basename(self.pdf_name)
 
     def get_problems_area(self, page: Page, accuracy = 1, offset = 1) -> Rect:
         #trim left and right
@@ -22,15 +23,13 @@ class KiceCropper:
         na = PdfUtils.pdf_clip_to_array(page, new_rect, fitz.Matrix(1, accuracy))
         na = na.mean(axis=1, dtype=np.uint32)
         na = np.where(na < 10, 0, na)
+        na = na.mean(axis=1, dtype=np.uint32)
         
-        upper_bound = 0
+        upper_bound = na.argmin()
         lower_bound = 0
         flag = False
-        for y in range(na.shape[0]):
-            if np.array_equal(na[y], [0,0,0]):
-                upper_bound = y
-                flag = True
-            elif flag and np.array_equal(na[y], [255,255,255]):
+        for y in range(upper_bound, na.shape[0]):
+            if na[y] == 255:
                 lower_bound = y
                 break
 
@@ -45,14 +44,16 @@ class KiceCropper:
         rects = []
         area = self.get_problems_area(page, accuracy)
 
+        mid = (area.x0 + area.x1) / 2
+
         left_area = Rect(area)
-        left_area.x0 = Ratio.mm_to_px(31)
-        left_area.x1 = Ratio.mm_to_px(143.5)
+        left_area.x1 = mid - Ratio.mm_to_px(5)
+        left_area.x0 = left_area.x1 - Ratio.mm_to_px(112.5)
         rects += self.get_problem_rects_from_area(page, left_area, accuracy)
 
         right_area = Rect(area)
-        right_area.x0 = Ratio.mm_to_px(154)
-        right_area.x1 = Ratio.mm_to_px(266.5)
+        right_area.x0 = mid + Ratio.mm_to_px(5)
+        right_area.x1 = right_area.x0 + Ratio.mm_to_px(112.5)
         rects += self.get_problem_rects_from_area(page, right_area, accuracy)
 
         return rects
@@ -110,7 +111,7 @@ class KiceCropper:
     def save_original(self) -> None:
         with fitz.open(self.pdf_name) as file:
             for i in range(len(self.infos)):
-                PdfUtils.extract_to_pdf(file, self.infos[i].page_num, self.infos[i].rect, f"output/original/{os.path.basename(self.pdf_name)[:-4]} {i+1}번 지1_original.pdf")
+                PdfUtils.extract_to_pdf(file, self.infos[i].page_num, self.infos[i].rect, f"output/original/{self.base_name[:-7]} {i+1}번 {self.base_name[-6:-4]}_original.pdf")
 
     def save_caption(self, caption_point: tuple, font_size: int) -> None:
         with fitz.open(self.pdf_name) as file:
@@ -124,5 +125,5 @@ class KiceCropper:
                 tw = fitz.TextWriter(new_page.rect)
                 tw.append((Ratio.mm_to_px(caption_point[0]), font_size), f"{os.path.basename(self.pdf_name)[:-4]} {i+1}번 지1", font, font_size)
                 tw.write_text(new_page, color=(0,0,0))
-                new_doc.save(f"output/caption/{os.path.basename(self.pdf_name)[:-4]} {i+1}번 지1_caption.pdf")
+                new_doc.save(f"output/caption/{self.base_name[:-7]} {i+1}번 {self.base_name[-6:-4]}_caption.pdf")
                 #PdfUtils.extract_to_pdf(file, infos[i].page_num, infos[i].rect, f"output/caption/{os.path.basename(pdf_name)[:-4]} {i+1}번 지1_caption.pdf")

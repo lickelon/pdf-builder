4345d9d3f0dd246a6705d7543f1a1b054f9931f4
unknown <jindoman0@naver.com>
2024-12-15 16:48:17 +0900

    feat: weekly solution overlayer

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from modules.kice_cropper import KiceCropper
 from modules.item_cropper import ItemCropper
+from modules.overlayer import Overlayer
 from pathlib import Path
 import fitz
+from utils.ratio import Ratio
 from utils.pdf_utils import PdfUtils
 from utils.solution_info import SolutionInfo
+from utils.overlay_object import *
+from utils.coord import Coord
 def kc_test():
     folder_path = Path('input')
     pdf_files = folder_path.glob('*.pdf')
@@ -28,15 +32,24 @@ def kc_test():
     new_doc.save("output/total.pdf")
 
 def ic_test():
-    with fitz.open('input/TH_Test.pdf') as file:
+    with fitz.open('input/E1lebKC211114.pdf') as file:
         ic = ItemCropper()
         #get problem
-        
+        ic.get_problem_rect_from_file(file, 10)
         #get solution
         ret = ic.get_solution_infos_from_file(file, 10)
-        for info in ret:
-            PdfUtils.extract_to_pdf(file, info.page_num, info.rect, f"output/{info.hexcode}.pdf")
+        print(ic.get_TF_of_solutions_from_file(file, 10))
+        print(ic.get_answer_from_file(file))
+
+def o_test():
+    new_doc = fitz.open()
+    o = Overlayer(new_doc)
+    o.add_page()
+    o.text_overlay(0, Coord(Ratio.mm_to_px(148.5), 100, 0), "Eulyoo1945-Regular.ttf", 10, "\ntest text", (1,0,0), fitz.TEXT_ALIGN_CENTER)
+    o.pdf_overlay(0, Coord(Ratio.mm_to_px(148), 90, 0), Component("input\TH_Problem_Test.pdf", 0, Rect(0,0,200,400), ComponentType.a))
+    new_doc.save("output/overlayertest.pdf")
 
 if __name__ == '__main__':
-    
+    from modules.weekly_sol import build
+    build()
     pass
--- modules/item_cropper.py
+++ modules/item_cropper.py
@@ -33,6 +33,10 @@ class ItemCropper:
         problem_rect = PdfUtils.trim_whitespace(page, area, Direction.DOWN, accuracy)
         return problem_rect
     
+    def get_problem_rect_from_file(self, file: fitz.Document, accuracy = 1) -> Rect:
+        page = file.load_page(0)
+        return self.get_problem_rect(page, accuracy)
+    
     def rgb_normalize(self, na : np.ndarray) -> np.ndarray:
         na = 255 - (((270 - na) // 30) * 30)
         na = np.where(na > 255, 0, na)
@@ -45,6 +49,10 @@ class ItemCropper:
         rect = Rect(174, 0, 274, 420)
         return Ratio.rect_mm_to_px(rect)
     
+    def get_TF_area(self):
+        rect = Rect(152, 0, 174, 420)
+        return Ratio.rect_mm_to_px(rect)
+
     def get_solution_bar_area(self):
         rect = Rect(140, 0, 142, 420)
         return Ratio.rect_mm_to_px(rect)
@@ -72,5 +80,44 @@ class ItemCropper:
         #srect = solutions["00FF00"].rect
         #text = page.get_text("text", clip=srect)
         #print(text[2:])
-
+        self.solutions = solutions
         return list(solutions.values())
+    
+    def get_TF_of_solutions_from_file(self, file: fitz.Document, accuracy = 1):
+        page = file.load_page(0)
+        rect = self.get_TF_area()
+        rectT = Rect(rect)
+        rectT.x1 = (rectT.x0 + rectT.x1) / 2
+        rectF = Rect(rect)
+        rectF.x0 = (rectF.x0 + rectF.x1) / 2
+
+        solutionTF = dict()
+        for sid, solution in self.solutions.items():
+            solution_rectT = Rect(rectT)
+            solution_rectT.y0 = solution.rect.y0
+            solution_rectT.y1 = solution.rect.y1
+            naT = PdfUtils.pdf_clip_to_array(page, solution_rectT, fitz.Matrix(accuracy, accuracy))
+            isT = (naT.mean(dtype=np.uint32) // 255) == 0
+
+            solution_rectF = Rect(rectF)
+            solution_rectF.y0 = solution.rect.y0
+            solution_rectF.y1 = solution.rect.y1
+            naF = PdfUtils.pdf_clip_to_array(page, solution_rectF, fitz.Matrix(accuracy, accuracy))
+            isF = (naF.mean(dtype=np.uint32) // 255) == 0
+
+            if isT^isF:
+                if isT:
+                    solutionTF[sid] = 1
+                elif isF:
+                    solutionTF[sid] = 0
+            else:
+                solutionTF[sid] = -1
+        
+        self.solutionTF = solutionTF
+        return solutionTF
+    
+    def get_answer_from_file(self, file: fitz.Document):
+        page = file.load_page(0)
+        srect = self.solutions["00FF00"].rect
+        text = page.get_text("text", clip=srect)
+        return text[2:3]
